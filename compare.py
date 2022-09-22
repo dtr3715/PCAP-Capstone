195df02609ceeb6be79755f9dc6ebc2d9e6e1e6f
GlossyCoin15 <91004595+jzp7326@users.noreply.github.com>
2022-09-22 16:03:03 -0400

    Added a PCAP comparer + sleeping between packets

---
+++ compare.py
@@ -0,0 +1,20 @@
+from scapy.all import *
+
+def compare_pcaps(pcap1_filepath, pcap2_filepath):
+	pcap1 = rdpcap(pcap1_filepath)
+	pcap2 = rdpcap(pcap2_filepath)
+	pcap1_len = len(pcap1)
+	pcap2_len = len(pcap2)
+	print(pcap1_filepath + ": " + str(pcap1_len) + " packets")
+	print(pcap2_filepath + ": " + str(pcap2_len) + " packets")
+	if pcap1_len != pcap2_len:
+		print("PCAPs not the same!")
+		return False
+	for i in range(0, pcap1_len):
+		if pcap1[i] != pcap2[i]:
+			print("Mismatch at packet " + str(i + 1) + "!")
+			return False
+	print("All packets contain the same data!")
+	return True
+	
+compare_pcaps("test.pcap", "test_output.pcap")
--- pcap_parser.go
+++ pcap_parser.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"math"
+	"time"
 
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/pcap"
@@ -13,32 +14,41 @@ func main() {
 
 	// Setup Input
 	// https://pkg.go.dev/github.com/google/gopacket/pcap#OpenOffline
-	pcapFileHandle, pcapFileErr := pcap.OpenOffline("icmp.pcap")
+	pcapFileHandle, pcapFileErr := pcap.OpenOffline("test.pcap")
 	if pcapFileErr != nil {
 		log.Fatal(pcapFileErr)
 		return
 	}
-	
+
 	// Setup Output
 	// https://pkg.go.dev/github.com/google/gopacket/pcap#OpenLive
 	pcapOutputHandle, pcapOutputErr := pcap.OpenLive("lo", math.MaxInt32, false, pcap.BlockForever)
 	if pcapOutputErr != nil {
-		log.Fatal(pcapFileErr)
+		log.Fatal(pcapOutputErr)
 		return
 	}
 
 	// Loop through all packets in pcapFileHandle
-	packetSource := gopacket.NewPacketSource(pcapFileHandle, pcapFileHandle.LinkType())
-	for packet := range packetSource.Packets() {
-	
-		fmt.Println(packet)
-		
+	var previousTimestamp time.Time
+	isFirstPacket := true
+	for packet := range gopacket.NewPacketSource(pcapFileHandle, pcapFileHandle.LinkType()).Packets() {
+
+		// Sleeps for the duration between this packet's and the previous packet's (Unless its the first packet, then no sleep)
+		if isFirstPacket {
+			isFirstPacket = false
+		} else {
+			time.Sleep(packet.Metadata().Timestamp.Sub(previousTimestamp))
+		}
+		previousTimestamp = packet.Metadata().Timestamp
+
 		// Write packet data to pcapOutputHandle
 		if pcapOutputErr = pcapOutputHandle.WritePacketData(packet.Data()); pcapOutputErr != nil {
 			fmt.Printf("[-] Error while sending: %s\n", pcapOutputErr.Error())
 			return
 		}
 	}
+
+	// Close handles
 	pcapFileHandle.Close()
 	pcapOutputHandle.Close()
 }
